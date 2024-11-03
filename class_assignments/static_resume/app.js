2cfbabc33bc78514fe1d43266f8cde2226ee9c86
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-11-03 19:18:41 +0500

    Initial commit

---
+++ class_assignments/static_resume/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+function toggle(){
+    var toggle = document.getElementById('skills')
+
+    if(toggle.style.display === 'none'){
+        toggle.style.display = 'block'
+    }else{
+        toggle.style.display = 'none'
+    }
+} 
